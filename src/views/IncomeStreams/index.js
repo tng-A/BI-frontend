9b4279ef1c2dec608f07c2da0aa4fb6aa44ed1a0
kwanj-k <mwangikelvin23@gmail.com>
2019-08-27 22:47:31 +0300

    call backend after the previous request is completed

--- src/views/IncomeStreams/index.js
+++ src/views/IncomeStreams/index.js
@@ -73,12 +73,18 @@ class Products extends Component {
     const {
       getFilteredIncomeStream,
       getPeriodsAction,
-      getMetricsActions
+      getMetricsActions,
+      loading
     } = this.props;
     getPeriodsAction();
     getMetricsActions();
     getFilteredIncomeStream({ ...this.state });
-    return
+    if (!loading){
+      setInterval(() => {
+        getFilteredIncomeStream({ ...this.state });
+      }, 15000);
+    }
+    return 
   }
 
   componentWillReceiveProps(nextProps) {
@@ -87,9 +93,6 @@ class Products extends Component {
     if (incomeStreams !== incomeStreamLate) {
       getFilteredIncomeStream({ ...this.state });
     }
-    return setInterval(() => {
-      getFilteredIncomeStream({ ...this.state });
-    }, 10000);
   }
   openModal() {
     this.setState({
