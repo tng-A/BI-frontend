66ffada284e6833b6c2b12bc179a7280d1687311
Erastus Ruiru <erastusruiru11@gmail.com>
2019-08-30 12:50:47 +0300

    fix bugs

--- src/redux/actionCreators/ValueCenter.js
+++ src/redux/actionCreators/ValueCenter.js
@@ -1,10 +1,13 @@
 import {
   GET_VALUE_CENTERS,
   GET_VALUE_CENTERS_SUCCESS,
-  GET_VALUE_CENTERS_FAILURE
+  GET_VALUE_CENTERS_FAILURE,
+  CREATE_VALUE_CENTER_TARGETS,
+  CREATE_VALUE_CENTER_TARGETS_SUCCESS,
+  CREATE_VALUE_CENTER_TARGETS_FAILURE
 } from "./../ActionTypes/ValueCenter";
 
-export const getValueCenter = (payload) => {
+export const getValueCenter = payload => {
   return {
     type: GET_VALUE_CENTERS,
     payload
@@ -18,5 +21,20 @@ export const getValueCenterSuccess = payload => ({
 
 export const getValueCenterfailure = error => ({
   type: GET_VALUE_CENTERS_FAILURE,
+  error
+});
+
+export const createValueCenterTargets = payload => ({
+  type: CREATE_VALUE_CENTER_TARGETS,
+  payload
+});
+
+export const createValueCenterTargetsSuccess = payload => ({
+  type: CREATE_VALUE_CENTER_TARGETS_SUCCESS,
+  payload
+});
+
+export const createValueCenterTargetsFailure = error => ({
+  type: CREATE_VALUE_CENTER_TARGETS_FAILURE,
   error
 });
--- src/redux/middleware/ValueCenter.js
+++ src/redux/middleware/ValueCenter.js
@@ -1,8 +1,12 @@
 import { put, call, takeEvery } from "redux-saga/effects";
+import { toast } from "react-toastify";
 import {
   getValueCenterSuccess,
   getValueCenterfailure,
-  getValueCenter
+  getValueCenter,
+  createValueCenterTargets,
+  createValueCenterTargetsSuccess,
+  createValueCenterTargetsFailure
 } from "../actionCreators/ValueCenter";
 import valueCenterService from "../../services/ValueCenter";
 
@@ -17,4 +21,29 @@ export function* getValueCentersAsync({ payload }) {
 
 export function* watchGetValueCenterPass() {
   yield takeEvery(getValueCenter().type, getValueCentersAsync);
+}
+
+export function* createValueCenterTargetsAsync({ payload }) {
+  try {
+    const response = yield call(
+      valueCenterService.createValueCenterTargets,
+      payload
+    );
+    yield put(createValueCenterTargetsSuccess({ ...response }));
+    toast.success("Target was created!", {
+      position: toast.POSITION.TOP_RIGHT
+    });
+  } catch (error) {
+    yield put(createValueCenterTargetsFailure(error));
+    toast.error("Error When creating target !", {
+      position: toast.POSITION.TOP_RIGHT
+    });
+  }
+}
+
+export function* createValueCenterTargetsWatcher() {
+  yield takeEvery(
+    createValueCenterTargets().type,
+    createValueCenterTargetsAsync
+  );
 }
